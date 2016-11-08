6fab8dffe84295dfe87cd42aff730acb7748299a
jbateman3 <github@zemborg.com>
2016-11-08 22:04:51 +0000

    Set pins 12 and 13 to be power for IR reciver

--- src/bootloader/irboot/irboot.c
+++ src/bootloader/irboot/irboot.c
@@ -121,6 +121,12 @@ __attribute__((OS_task)) int main_irboot(void)
 	}
 
 
+
+	DDRB |=  0b00110000;
+	PORTB &= 0b00010000;
+	PORTB |= 0b00100000;
+
+
 #ifdef DEBUG_IO
 	// Set the pins as outputs
 	DDRD |= 0b11110000;
